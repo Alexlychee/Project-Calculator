6fcaac7d7f11fd794020b28b3f6feec6c5d11969
Alex Ly <aly26@csu.fullerton.edu>
2024-08-22 19:18:41 -0700

    added basic html

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let firstNum;
+let secondNum;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -11,5 +15,21 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if ((a == 0) || (b == 0)) {
+        return "Cannot divide by zero";
+    }
     return a / b;
+}
+
+function operate(operator, firstNum, secondNum) {
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+    }
 }
